c92325ea6c4463357a61ef24d9af58778342507e
wkm-wangZhe <clxy_wz@aliyun.com>
2022-05-25 12:39:26 +0800

    webpack5 +ts + postcss

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -1,55 +1,35 @@
 const webpack = require('webpack')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.config.js')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin }  = require('clean-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const WebpackBar = require("webpackbar");
+
+
+
+
 const path = require('path');
 
 
-function resolve (dir) {
-    return path.join(__dirname,  dir)
+function resolve(dir) {
+    return path.join(__dirname, dir)
 }
 
 const devWebpackConfig = merge(baseWebpackConfig, {
     mode: 'production',
+    devtool: false,
     output: {
         publicPath: './',
         filename: 'static/js/[name]_[contenthash:8].js'
     },
-    devtool: 'cheap-module-souce-map',
     plugins: [
-        new WebpackBar(),
-        new CleanWebpackPlugin(),
+
+
         new webpack.DefinePlugin({
-            'process.env': { 
+            'process.env': {
                 NODE_ENV: '"production"',
                 REACT_APP_API: '"product"'
-            } 
-        }),
-       
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            manifest: '../public/manifest.json',
-            favicon: 'public/favicon.ico',
-            template: resolve("../public/index.html"),
-            inject: true,
-        }),
-        new CopyWebpackPlugin([
-            {
-              from: resolve('../public'),
-              to:  resolve('../dist'),
-              ignore: ['.*']
             }
-        ]),
-        new OptimizeCSSAssetsPlugin(),
-     
-        // copy custom static assets
-  
+        })
     ],
-   
+
 })
 
 // //console.log('devWebpackConfig', devWebpackConfig)
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import App from './app';
+// polyfill
+// webpack4 自动加载了, webpack5 需要自己配置
+import "core-js/stable"; // 负责处理ECMAScript的核心语法， 如propmise 
+import "regenerator-runtime/runtime" //   负责处理generator语法
 
 const render = Component => {
     ReactDOM.render(
