f0ab48a0f90a3ec8ebb3be34368146f10e88a52c
zhaoqiaochu <zhaoqiaochu@rinna.co.jp>
2022-05-25 16:10:33 +0800

    feat: add dev mock middleware

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,6 +3,9 @@ const { merge } = require("webpack-merge");
 const baseWebpackConfig = require("./webpack.config.js");
 const path = require("path");
 // const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const { mockMiddleware } = require('../tools/middleware.js')
+const fs = require('fs')
+
 function resolve(dir) {
 	return path.join(__dirname, dir);
 }
@@ -20,10 +23,23 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 		// historyApiFallback: {
 		//   disableDotRule: true,
 		// }
+
+    // webpack-dev-server4.x
+    // onBeforeSetupMiddleware (devServer) {
+    //   const mockFile = path.join(__dirname, '../tools/mock.js')
+    //   if (fs.existsSync(mockFile)) {
+    //     devServer.app.use(mockMiddleware(mockFile))
+    //   }
+    // }
+
+    // webapck-dev-server3.x
+    before (app) {
+      const mockFile = path.join(__dirname, '../tools/mock.js')
+      if (fs.existsSync(mockFile)) {
+        app.use(mockMiddleware(mockFile))
+      }
+    }
 	},
-	// devServer: {
-	//   hot: true
-	// },
 	plugins: [
 		// new ReactRefreshWebpackPlugin(),
 		// new CleanWebpackPlugin(),
