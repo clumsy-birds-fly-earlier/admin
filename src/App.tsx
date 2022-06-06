33a84728e2a531f17240ab461236925383235bd8
ManiacHanz <182490078@qq.com>
2022-06-06 15:24:43 +0800

    Chore: 测试husky hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { FC, useEffect, useState } from 'react';
 // import axios from "axios";
 import styled from 'styled-components';
+// 用于测试jest转译样式文件没毛病
+import './index.css';
 
 const Warp = styled.div`
   color: #000;
   font-size: 24px;
 `;
-// 用于测试jest转译样式文件没毛病
-import './index.css';
 
 const App: FC = () => {
   const [count, setCount] = useState<number>(0);
@@ -18,7 +18,7 @@ const App: FC = () => {
   }, []);
   return (
     <Warp>
-      <div>{count}</div>
+      <div>{count}123</div>
       <button className="button" onClick={() => setCount((c) => c + 1)}>
         add
       </button>
