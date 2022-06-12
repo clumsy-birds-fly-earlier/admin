e519703692d4ce0914395561ff2204becf793e7b
ManiacHanz <182490078@qq.com>
2022-06-12 16:04:48 +0800

    Chore: 测试vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React, { FC, useEffect, useState } from 'react';
-// import axios from "axios";
 import styled from 'styled-components';
+// 用于测试jest转译样式文件没毛病
+import './index.css';
 
 const Warp = styled.div`
   color: #000;
   font-size: 24px;
 `;
-// 用于测试jest转译样式文件没毛病
-import './index.css';
 
 const App: FC = () => {
   const [count, setCount] = useState<number>(0);
