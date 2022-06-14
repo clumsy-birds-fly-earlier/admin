3657355c7a875bfaf4b37109ac432f3575c2b046
ManiacHanz <182490078@qq.com>
2022-06-14 22:10:02 +0800

    Chore: test vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React, { FC, useEffect, useState } from 'react';
+import {RouteProps} from 'react-router'
 // import axios from "axios";
 import styled from 'styled-components';
+// 用于测试jest转译样式文件没毛病
+import './index.css';
 
 const Warp = styled.div`
   color: #000;
   font-size: 24px;
 `;
-// 用于测试jest转译样式文件没毛病
-import './index.css';
 
-const App: FC = () => {
+
+
+type AppProps = RouteProps & {
+  name: string;
+}
+
+const App: FC<AppProps> = (props) => {
+  console.log(props.location)
   const [count, setCount] = useState<number>(0);
 
   useEffect(() => {
@@ -19,7 +27,7 @@ const App: FC = () => {
   return (
     <Warp>
       <div>{count}</div>
-      <button className="button" onClick={() => setCount((c) => c + 1)}>
+      <button className="button" onClick={() => setCount((c) => c + 2)}>
         add
       </button>
     </Warp>
